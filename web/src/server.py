5bf952e83be5b95da691fce0cf5600a7b506fcc6
andrew hernandez <anh190@ucsd.edu>
2021-04-30 00:14:20 -0700

    switched to flask

--- web/src/server.py
+++ web/src/server.py
@@ -2,6 +2,10 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
 
+from flask import Flask
+from flask import Response, request, redirect, render_template
+import requests
+
 import mysql.connector as mysql
 import os
 
@@ -10,7 +14,7 @@ db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']
 db_host = os.environ['MYSQL_HOST']
 
-def get_home(req):
+'''def get_home(req):
   # Connect to the database and retrieve the users
   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
   cursor = db.cursor()
@@ -18,10 +22,10 @@ def get_home(req):
   records = cursor.fetchall()
   db.close()
 
-  return render_to_response('templates/home.html', {'users': records}, request=req)
+  return render_to_response('templates/home.html', {'users': records}, request=req)'''
 
 ''' Route Configurations '''
-if __name__ == '__main__':
+'''if __name__ == '__main__':
   config = Configurator()
 
   config.include('pyramid_jinja2')
@@ -34,4 +38,13 @@ if __name__ == '__main__':
 
   app = config.make_wsgi_app()
   server = make_server('0.0.0.0', 6000, app)
-  server.serve_forever()
+  server.serve_forever()'''
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_home():
+  return render_template('home.html')
+
+if __name__ == '__main__':
+  app.run(debug = True,port=6000)
