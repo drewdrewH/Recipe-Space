625fdf233c37c0bd2a80692e40d32591bc1e302a
andrew hernandez <anh190@ucsd.edu>
2021-04-30 20:43:43 -0700

    more modal updates

--- web/src/public/modal.js
+++ web/src/public/modal.js
@@ -2,49 +2,49 @@
 /* adapted from/Credit: open source https://codepen.io/thebeloved88/pen/zvrzoE */
 /*Title: Cool Modal Popup Sign In/Out Form*/
 
-$(function() {
+
     //defining all needed variables
-    var $overlay = $('.overlay');
-    var $mainPopUp = $('.SignUpModal')
-    var $signIn = $('#sign-in');
-    var $SignUp = $('#SignUp');
-    var $formSignIn = $('form.sign-in');
-    var $formSignUp = $('form.SignUp');
+    var overlay = document.getElementsByClassName('overlay');
+    var mainPopUp = document.getElementsByClassName('SignUpModal')
+    var signIn = document.getElementById('sign-in');
+    var SignUp = document.getElementById('SignUp');
+    var formSignIn = document.getElementsByClassName('sign-in');
+    var formSignUp = document.getElementsByClassName('SignUp');
     
    
    
     
-    $('button').on('click', function(){
-      $overlay.addClass('visible');
-      $mainPopUp.addClass('visible');
-      $signIn.addClass('active');
-      $SignUp.removeClass('active');
-      $formSignUp.removeClass('move-left');
-      $formSignIn.removeClass('move-left');
+    document.getElementById('modalButton').on('click', function(){
+      overlay.addClass('visible');
+      mainPopUp.addClass('visible');
+      signIn.addClass('active');
+      SignUp.removeClass('active');
+      formSignUp.removeClass('move-left');
+      formSignIn.removeClass('move-left');
     });
-    $overlay.on('click', function(){
-      $(this).removeClass('visible');
-      $mainPopUp.removeClass('visible');
+    overlay.on('click', function(){
+      (this).removeClass('visible');
+      mainPopUp.removeClass('visible');
     });
-    $('#popup-close-button a').on('click', function(e){
+    document.getElementsById('popup-close-button').on('click', function(e){
       e.preventDefault();
-      $overlay.removeClass('visible');
-      $mainPopUp.removeClass('visible');
+      overlay.removeClass('visible');
+      mainPopUp.removeClass('visible');
     });
     
-    $signIn.on('click', function(){
-      $signIn.addClass('active');
-      $SignUp.removeClass('active');
-      $formSignIn.removeClass('move-left');
-      $formSignUp.removeClass('move-left');
+    signIn.on('click', function(){
+      signIn.addClass('active');
+      SignUp.removeClass('active');
+      formSignIn.removeClass('move-left');
+      formSignUp.removeClass('move-left');
     });
     
-    $SignUp.on('click', function(){
-      $signIn.removeClass('active');
-      $SignUp.addClass('active');
-      $formSignIn.addClass('move-left');
-      $formSignUp.addClass('move-left');
+    SignUp.on('click', function(){
+      signIn.removeClass('active');
+      SignUp.addClass('active');
+      formSignIn.addClass('move-left');
+      formSignUp.addClass('move-left');
     });
     
     
-  });
+ 
