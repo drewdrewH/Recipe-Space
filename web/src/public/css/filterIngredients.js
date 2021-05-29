3515ecb413b256020f9580b9295faa300e380a5b
Fred <fryang@ucsd.edu>
2021-05-29 15:13:28 -0700

    cleaner way of removing ingredient tags from filter

--- web/src/public/css/filterIngredients.js
+++ web/src/public/css/filterIngredients.js
@@ -1,12 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     var ingredientTags = document.getElementById('ingredient_tags');
+    var filterInput = document.getElementById('filter-search-val');
+    var autocomplete = document.getElementById('search_autocomplete');
+    var filteredIngredients = [];
 
     ingredientTags.addEventListener("click", function(e) {
         if(e.target && e.target.classList.contains("ingredient_tag")){
             var ingredient = e.target.innerHTML;
             console.log(ingredient);
             var filteredIngredientsTemp = [];
-            document.getElementById('hidden').innerHTML.split(',').forEach(element => filteredIngredientsTemp.push(element));
+            var ingredientTag = ingredientTags.getElementsByClassName("ingredient_tag");
+            for(i = 0; i < ingredientTag.length; i++) {
+                //console.log(ingredientTag[i].innerHTML);
+                filteredIngredientsTemp.push(ingredientTag[i].innerHTML);
+            }
             const index = filteredIngredientsTemp.indexOf(ingredient);
             filteredIngredientsTemp.splice(index, 1);
             if(filteredIngredientsTemp.length == 1) {
@@ -21,79 +28,78 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-});
-
-var filterInput = document.getElementById('filter-search-val');
-var autocomplete = document.getElementById('search_autocomplete');
-var filteredIngredients = [];
-
-autocomplete.addEventListener("click", function(e) {
-    if(e.target){
-        var searchText = e.target.innerHTML;
-        console.log(document.getElementById('hidden').innerHTML);
-        document.getElementById('hidden').innerHTML.split(',').forEach(element => filteredIngredients.push(element))
-        if(searchText != "" && !filteredIngredients.includes(searchText)){
-            filteredIngredients.push(searchText);
+    autocomplete.addEventListener("click", function(e) {
+        if(e.target){
+            var searchText = e.target.innerHTML;
+            var ingredientTag = ingredientTags.getElementsByClassName("ingredient_tag");
+            for(i = 0; i < ingredientTag.length; i++) {
+                //console.log(ingredientTag[i].innerHTML);
+                filteredIngredients.push(ingredientTag[i].innerHTML);
+            }
+            if(searchText != "" && !filteredIngredients.includes(searchText)){
+                filteredIngredients.push(searchText);
+            }
+            filteredIngredients = filteredIngredients.filter(String);
+            location.href = "/filter_ingredient/"+filteredIngredients;
         }
-        filteredIngredients = filteredIngredients.filter(String);
-        location.href = "/filter_ingredient/"+filteredIngredients;
-    }
-    else {
-        console.log("nothing");
-    }
-});
-
-filterInput.addEventListener('input', (event) =>{
-    var searchText = filterInput.value;
-    searchText = removeBadChars(searchText);
-    if(searchText != ""){
-        fetch('/search_autocomplete/'+searchText)
-            .then(response => response.json())
-            .then(function(data) {
-                console.log(data);
-                var ingredient = data['ingredients']
-                if(ingredient == 'no records') {
-                    document.getElementById('autocomplete_entry'+String(1)).innerHTML = "no matching ingredients";
-                    for (var i = 1; i < 10; i++) {
-                        document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = "";
-                    }
-                    document.getElementById('searchbar').style.borderBottomRightRadius = '0px';
-                    document.getElementById('searchbar').style.borderBottomLeftRadius = '0px';
-                    document.querySelector('.search_autocomplete').style.borderBottom = '1px solid hsl(120, 100%, 35%)';
-                }
-                else if(ingredient == 'no search') {
-                    console.log("no search");
-                    document.getElementById('searchbar').style.borderBottomRightRadius = '20px';
-                    document.getElementById('searchbar').style.borderBottomLeftRadius = '20px';
-                    document.querySelector('.search_autocomplete').style.borderBottom = 'none';
-                }
-                else {
-                    for (var i = 0; i < 10; i++) {
-                        if(ingredient[i] == undefined) {
+        else {
+            console.log("nothing");
+        }
+    });
+    
+    filterInput.addEventListener('input', (event) =>{
+        var searchText = filterInput.value;
+        searchText = removeBadChars(searchText);
+        if(searchText != ""){
+            fetch('/search_autocomplete/'+searchText)
+                .then(response => response.json())
+                .then(function(data) {
+                    console.log(data);
+                    var ingredient = data['ingredients']
+                    if(ingredient == 'no records') {
+                        document.getElementById('autocomplete_entry'+String(1)).innerHTML = "no matching ingredients";
+                        for (var i = 1; i < 10; i++) {
                             document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = "";
-                        }
-                        else {
-                            document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = ingredient[i];
                         }
                         document.getElementById('searchbar').style.borderBottomRightRadius = '0px';
                         document.getElementById('searchbar').style.borderBottomLeftRadius = '0px';
                         document.querySelector('.search_autocomplete').style.borderBottom = '1px solid hsl(120, 100%, 35%)';
                     }
-                }
-            })
-    }
-    else {
-        for (var i = 0; i < 10; i++) {
-            document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = "";
-            document.getElementById('searchbar').style.borderBottomRightRadius = '20px';
-            document.getElementById('searchbar').style.borderBottomLeftRadius = '20px';
-            document.querySelector('.search_autocomplete').style.borderBottom = 'none';
+                    else if(ingredient == 'no search') {
+                        console.log("no search");
+                        document.getElementById('searchbar').style.borderBottomRightRadius = '20px';
+                        document.getElementById('searchbar').style.borderBottomLeftRadius = '20px';
+                        document.querySelector('.search_autocomplete').style.borderBottom = 'none';
+                    }
+                    else {
+                        for (var i = 0; i < 10; i++) {
+                            if(ingredient[i] == undefined) {
+                                document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = "";
+                            }
+                            else {
+                                document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = ingredient[i];
+                            }
+                            document.getElementById('searchbar').style.borderBottomRightRadius = '0px';
+                            document.getElementById('searchbar').style.borderBottomLeftRadius = '0px';
+                            document.querySelector('.search_autocomplete').style.borderBottom = '1px solid hsl(120, 100%, 35%)';
+                        }
+                    }
+                })
         }
+        else {
+            for (var i = 0; i < 10; i++) {
+                document.getElementById('autocomplete_entry'+String(i+1)).innerHTML = "";
+                document.getElementById('searchbar').style.borderBottomRightRadius = '20px';
+                document.getElementById('searchbar').style.borderBottomLeftRadius = '20px';
+                document.querySelector('.search_autocomplete').style.borderBottom = 'none';
+            }
+        }
+    })
+    
+    function removeBadChars(ingredient){
+        const badChars = ['\\', '#', '/', '.', '\''];
+        badChars.forEach(char => ingredient = ingredient.replaceAll(char, ''));
+        return ingredient;
     }
-})
 
-function removeBadChars(ingredient){
-    const badChars = ['\\', '#', '/', '.', '\''];
-    badChars.forEach(char => ingredient = ingredient.replaceAll(char, ''));
-    return ingredient;
-}
+});
