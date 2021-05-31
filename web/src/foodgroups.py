7e64eef81c27e7686be8f8c640e9f3a83690444e
andrew hernandez <anh190@ucsd.edu>
2021-05-30 22:55:45 -0700

    added bookmarking to profile and browse page

---
+++ web/src/foodgroups.py
@@ -0,0 +1,865 @@
+groups = {'vegetables': """Acorn Squash
+Ahipa
+Amaranth
+American Groundnut
+Aonori
+Arame
+Arracacha
+Arrowroot
+Artichoke
+Arugula
+Asparagus
+Adzuki Bean
+
+Vegetables that start with B are:
+
+Bamboo Shoots
+Banana Squash
+Beetroot
+Belgian Endive
+Bell Peppers
+Black Eyed Pea
+Black Radish
+Black Salsify
+Bok Choy
+Broadleaf Arrowhead
+Broccoflower
+Broccoli
+Broccolini
+Brussel Sprouts
+Burdock Roots
+Buttercup Squash
+Butternut Squash
+
+Vegetables that start with C are:
+
+Cabbage
+Cactus
+Camas
+Canna
+Caper
+Cardoon
+Carrot
+Cassava
+Catsear
+Cauliflower
+Celeriac
+Celery
+Celtuce
+Chaya
+Chayote Squash
+Cherry Tomato
+Chick Pea
+Chickweed
+Chicory
+Chives
+Chrysanthemum
+Collard Greens
+Common Beans
+Crookneck Squash
+Common Purslane
+Courgette Flowers
+Cress
+Cucumbers
+
+Vegetables that start with D are:
+
+Dabberlocks
+Daikon
+Dandelion
+Delicata Squash
+Daylily
+Dill
+Dolichos
+Drumstick
+Dulse
+
+Vegetables that start with E are:
+
+Earthnut Pea
+Eggplant
+Elephant Foot Yam
+Elephant Garlic
+Endive
+Ensete
+
+Vegetables that start with F are:
+
+Fat Hen
+Fava bean
+Fennel
+Fiddlehead Green
+Florence Fennel
+Fluted Pumpkin
+
+Vegetables that start with G are:
+
+Galangal
+Garbanzo
+Garden Rocket
+Garlic
+Garlic Chives
+Ginger
+Golden Samphire
+Good King Henry
+Greater Plantain
+Green Beans
+Green Soybeans
+Guar
+
+Vegetables that start with H are:
+
+Hamburg Parsley
+Hijiki
+Horse Gram
+Horseradish
+
+Vegetables that start with I are:
+
+Indian Pea
+
+Vegetables that start with J are:
+
+None
+
+Vegetables that start with K are:
+
+Kale
+Kohlrabi
+Komatsuna
+Kombu
+Kurrat
+
+Vegetables that start with L are:
+
+Lagos Bologi
+Land Cress
+Laver
+Leek
+Lemongrass
+Lentil
+Lettuce
+Lima Bean
+Lotus Root
+
+Vegetables that start with M are:
+
+Malabar Spinach
+Mangetout
+Manoa
+Mashua
+Mulukhiyah
+Mizuna
+Morel Mushrooms
+Moth Bean
+Mozuku
+Mung Bean
+Mushrooms
+Mustard Greens
+
+Vegetables that start with N are:
+
+Napa Cabbage
+New Zealand Spinach
+Nopal
+Nori
+
+Vegetables that start with O are:
+
+Ogonori
+Okra
+Onion
+Orache
+
+Vegetables that start with P are:
+
+Pak Choy
+Paracress
+Parsnip
+Peas
+Pearl Onion
+Pigeon Pea
+Pignut
+Potato
+Prussian Asparagus
+Prairie Turnip
+Pumpkin
+
+Vegetables that start with Q are:
+
+None
+
+Vegetables that start with R are:
+
+Radicchio
+Radish
+Ramp
+Rapini
+Red Leaf Lettuce
+Ricebean
+Runner Bean
+Rutabaga
+
+Vegetables that start with S are:
+
+Salad Savoy
+Salsify
+Samphire
+Scorzonera
+Sculpit
+Sea Beet
+Sea Grape
+Sea Kale
+Sea Lettuce
+Shallot
+Sierra Leone Bologi
+Skirret
+Snap Pea
+Snow Pea
+Soko
+Sorrel
+Sour Cabbage
+Soybean
+Spinach
+Spring Onion
+Scallion
+Squash Blossoms
+Summer Squash
+Swede
+Sweet Potato
+
+Vegetables that start with T are:
+
+Taro
+Tarwi
+Tatsoi
+Tepary Bean
+Tigernut
+Tomatillo
+Tomato
+Tree Onion
+Turmeric
+Turnip
+U:
+Vegetables that start with U are:
+
+Urad Bean
+Ulluco
+V:
+Vegetables that start with V are:
+
+None
+W:
+Vegetables that start with W are:
+
+Wasabi
+Water Chestnut
+Water Caltrop
+Water Spinach
+Watercress
+Welsh Onion
+Wheatgrass
+Wild Leek
+Winged Bean
+Winter Squash
+
+
+
+Yam
+Yao Choy
+Yardlong Bean
+Yarrow
+Yuca Root
+Yukon Gold Potatoes
+
+
+Zucchini""", 'dairy': """ Acidophilus Milk
+Ammonium Caseinate
+Butter
+Butter Esters
+Butter Fat
+Butter Oil
+Butter Solids
+Buttermilk
+Buttermilk Powder
+Calcium Caseinate
+Casein
+Caseinate (in general)
+Cheese (All animal-based)
+Condensed Milk
+Cottage Cheese
+Cream
+Curds
+Custard
+Delactosed Whey
+Demineralized Whey
+Dry Milk Powder
+Dry Milk Solids
+Evaporated Milk
+Ghee (see page 109 in Go Dairy Free)
+Goat Cheese
+Goat Milk
+Half & Half
+Hydrolyzed Casein
+Hydrolyzed Milk Protein
+Iron Caseinate
+Lactalbumin
+Lactoferrin
+Lactoglobulin
+Lactose
+Lactulose
+Low-Fat Milk
+Magnesium Caseinate
+Malted Milk
+Milk
+Milk Derivative
+Milk Fat
+Milk Powder
+Milk Protein
+Milk Solids
+Natural Butter Flavor
+Nonfat Milk
+Nougat
+Paneer
+Potassium Caseinate
+Pudding
+Recaldent
+Rennet Casein
+Sheep Milk
+Sheep Milk Cheese
+Skim Milk
+Sodium Caseinate
+Sour Cream
+Sour Milk Solids
+Sweetened Condensed Milk
+Sweet Whey
+Whey
+Whey Powder
+Whey Protein Concentrate
+Whey Protein Hydrolysate
+Whipped Cream
+Whipped Topping
+Whole Milk
+Yogurt
+Zinc Caseinate""", 'fruits':"""Abiu
+Açaí
+Acerola
+Ackee
+African cucumber
+Apple
+Apricot
+Avocado
+Banana
+Bilberry
+Blackberry
+Blackcurrant
+Black sapote
+Blueberry
+Boysenberry
+Breadfruit
+Buddha's hand (fingered citron)
+Cactus pear
+Canistel
+Cempedak
+Cherimoya (Custard Apple)
+Cherry
+Chico fruit
+Cloudberry
+Coco De Mer
+Coconut
+Crab apple
+Cranberry
+Currant
+Damson
+Date
+Dragonfruit (or Pitaya)
+Durian
+Egg Fruit
+Elderberry
+Feijoa
+Fig
+Finger Lime (or Caviar Lime)
+Goji berry
+Gooseberry
+Grape
+Raisin
+Grapefruit
+Grewia asiatica (phalsa or falsa)
+Guava
+Hala Fruit
+Honeyberry
+Huckleberry
+Jabuticaba
+Jackfruit
+Jambul
+Japanese plum
+Jostaberry
+Jujube
+Juniper berry
+Kaffir Lime
+Kiwano (horned melon)
+Kiwifruit
+Kumquat
+Lemon
+Lime
+Loganberry
+Longan
+Loquat
+Lulo
+Lychee
+Magellan Barberry
+Mamey Apple
+Mamey Sapote
+Mango
+Mangosteen
+Marionberry
+Melon
+Cantaloupe
+Galia melon
+Honeydew
+Mouse Melon
+Watermelon
+Miracle fruit
+Monstera deliciosa
+Mulberry
+Nance
+Nectarine
+Orange
+Blood orange
+Clementine
+Mandarine
+Tangerine
+Papaya
+Passionfruit
+Peach
+Pear
+Persimmon
+Plantain
+Plum
+Prune (dried plum)
+Pineapple
+Pineberry
+Plumcot (or Pluot)
+Pomegranate
+Pomelo
+Purple mangosteen
+Quince
+Raspberry
+Salmonberry
+Rambutan (or Mamin Chino)
+Redcurrant
+Rose apple
+Salal berry
+Salak
+Satsuma
+Shine Muscat or Vitis Vinifera
+Sloe or Hawthorn Berry
+Soursop
+Star apple
+Star fruit
+Strawberry
+Surinam cherry
+Tamarillo
+Tamarind
+Tangelo
+Tayberry
+Tomato
+Ugli fruit
+White currant
+White sapote
+Yuzu""", 'grains': """Amaranth
+Barley
+flour
+baking
+Hulled Barley/Barley Groats
+Pot/Scotch Barley
+Pearl/Pearled Barley
+Barley Flakes
+Barley Grits
+Buckwheat
+Buckwheat Groats/Unroasted Buckwheat
+Kasha/Roasted Buckwheat
+Buckwheat Grits
+Corn
+Hominy
+Popcorn
+Millet
+Oats
+Oat Groats
+Rolled Oats/Old Fashioned Oatmeal
+Steel cut oats/Irish Oatmeal
+Quick Cooking Oats/Quick Oatmeal
+Instant Oats/Instant Oatmeal
+Oat Bran
+Quinoa
+Rice- see Rice List
+Rye
+Rye Berries
+Cracked Rye
+Rye Flakes
+Sorghum
+Spelt
+Spelt Berries
+Spelt Flakes
+Teff
+Triticale
+Triticale Berries
+Triticale Flakes
+Wheat
+Wheat Berries
+Bulgur Wheat
+Cracked Wheat
+Farina
+Semolina
+Pasta
+Couscous
+Wheat Bran
+Wheat Flakes
+Seitan/Wheat Gluten
+
+Farro(variety of Wheat)
+Kamut(Brand and Trademarked Name of Khorasan Wheat)
+Durum Wheat(variety of Wheat)
+Wild Rice""", 'meats':"""
+American Bison
+Antelope
+beef
+Barbary Sheep
+Bighorn sheep
+Bushpig
+Camel
+Capybara
+Caraba
+Caribou (reindeer)
+Cattle
+Dall Sheep
+Deer
+Domestic Goat
+Domestic pig
+Domestic Sheep
+Eared seal
+Earless seal
+Groundhog
+Guinea pig
+Hare
+Ibex
+Kangaro
+Lamb
+Llama
+Moose
+Mountain Goat
+Opossum
+Peccary (Javelina)
+Pika
+pork
+sausage
+
+Rabbit
+Red River Hog
+Veal (calves)
+Wallaby
+Walrus
+Wild boar
+Wild goat
+Yak
+ """, 'seafood':"""
+Anchovy
+Basa
+Bass
+Catfish
+Carp
+Cod
+Crappie
+Eel
+Flounder
+Grouper
+Haddock
+Halibut
+Herring
+Kingfish
+Mackerel
+Mahi Mahi
+Marlin
+Orange Roughy
+Perch
+Pike
+Pollock
+Salmon
+Sardine
+Snapper
+Sole
+Swordfish
+Tilapia
+Trout
+Tuna
+Walleye
+ 
+Shellfish:
+Abalone
+Clam
+Conch
+Crab
+Crayfish
+Cuttlefish
+Lobster
+Loc
+Mussel
+Octopus
+Prawn
+Oyster
+Scallop
+Shrimp
+Snail (escargots)
+ 
+Canned Fish Such As:
+Anchovies
+Clams
+Tuna
+Sardines """
+, 'poultry':"""
+Chicken
+Cornish Hen
+Dove
+Duck
+Emu
+Goose
+Grouse
+Guinea fowl
+New World quail
+Ostrich
+Partridge
+Pheasant
+Pigeon
+Quail
+Turkey
+ 
+ 
+Eggs
+Chicken Eggs
+Duck Eggs
+Goose Eggs
+Hen Eggs
+Quail Eggs""",
+
+'seasonings':"""Ajwain, carom seeds (Trachyspermum ammi) (Pakistan, South Asia, India, Afghanistan, Iran, Egypt, Eritrea & Ethiopia)
+Akudjura (Solanum centrale) (Australia)
+Alexanders (Smyrnium olusatrum)
+Alkanet (Alkanna tinctoria), for red color
+Alligator pepper, mbongo spice (mbongochobi), hepper pepper (Aframomum danielli, A. citratum, A. exscapum) (West Africa)
+Allspice (Pimenta dioica)
+Angelica (Angelica archangelica)
+Anise (Pimpinella anisum)
+Aniseed myrtle (Syzygium anisatum) (Australia)
+Annatto (Bixa orellana)
+Artemisia (Artemisia spp.)
+Asafoetida (Ferula assafoetida)
+Avens (Geum urbanum)
+Avocado leaf (Persea americana)
+B
+Barberry (Berberis vulgaris and other Berberis spp.)
+Basil, sweet (Ocimum basilicum)
+Basil, Holy (Ocimum tenuiflorum)
+Basil, lemon (Ocimum × citriodorum)
+Basil, Thai (O. basilicum var. thyrsiflora)
+Bay leaf (Laurus nobilis)
+Indian Bay leaf, tejpat, malabathrum (Cinnamomum tamala)
+Boldo (Peumus boldus)
+Borage (Borago officinalis)
+Blue fenugreek, blue melilot (Trigonella caerulea)
+C
+California bay laurel (Umbellularia californica)
+Caper (Capparis spinosa)
+Caraway (Carum carvi)
+Cardamom (Elettaria cardamomum)
+Cardamom, black; badi ilaichi (Amomum subulatum, Amomum costatum) (India, Pakistan)
+Cassia (Cinnamomum aromaticum)
+Catnip (Nepeta cataria)
+Cao guo, Chinese black cardamom (Lanxangia tsaoko) (China)
+Cayenne pepper (Capsicum annuum)
+Celery leaf (Apium graveolens)
+Celery seed (Apium graveolens)
+Chervil (Anthriscus cerefolium)
+Cichοrium spinosum
+Chicory (Cichorium intybus)
+Chili pepper (Capsicum spp.)
+Chives (Allium schoenoprasum)
+Cicely, sweet cicely (Myrrhis odorata)
+Cilantro, coriander greens, coriander herb, Chinese parsley (Coriandrum sativum)
+Cinnamon, Indonesian (Cinnamomum burmannii, Cassia vera)
+Cinnamon, Saigon or Vietnamese (Cinnamomum loureiroi)
+Cinnamon, true or Ceylon (Cinnamomum verum, C. zeylanicum)
+Cinnamon, white (Canella winterana)
+Cinnamon myrtle (Backhousia myrtifolia) (Australia)
+Clary, Clary sage (Salvia sclarea)
+Clove (Syzygium aromaticum)
+Coriander seed (Coriandrum sativum)
+Coriander, Vietnamese (Persicaria odorata)
+Costmary (Tanacetum balsamita)
+Cubeb pepper (Piper cubeba)
+Cudweed (Gnaphalium spp.) (Vietnam)
+Culantro, culangot, long coriander, recao (Eryngium foetidum)
+Cumin (Cuminum cyminum)
+Curry leaf (Murraya koenigii)
+Curry plant (Helichrysum italicum)
+D
+Dill herb or weed (Anethum graveolens)
+Dill seed (Anethum graveolens)
+E
+Elderflower (Sambucus spp.)
+Epazote (Dysphania ambrosioides)
+F
+Fennel (Foeniculum vulgare)
+Fenugreek (Trigonella foenum-graecum)
+Filé powder, gumbo filé (Sassafras albidum)
+Fingerroot, temu kuntji, krachai, k'cheay (Boesenbergia rotunda) (Java, Thailand, Cambodia)
+G
+Galangal, greater (Alpinia galanga)
+Galangal, lesser (Alpinia officinarum)
+Galingale (Cyperus spp.)
+Garlic (Allium sativum)
+Garlic chives (Allium tuberosum)
+Ginger (Zingiber officinale)
+Ginger, torch, bunga siantan (Etlingera elatior) (Indonesia)
+Golpar, Persian hogweed (Heracleum persicum) (Iran)
+Grains of paradise (Aframomum melegueta)
+Grains of Selim, Kani pepper (Xylopia aethiopica)
+H
+Hoja santa, hierba santa, acuyo (Piper auritum) (Mexico)
+Horseradish (Armoracia rusticana)
+Houttuynia cordata (Vietnam)
+Huacatay, Mexican marigold, mint marigold (Tagetes minuta)
+Hyssop (Hyssopus officinalis)
+I
+Indonesian bay leaf, daun salam (Syzygium polyanthum)
+J
+Jasmine flowers (Jasminum spp.)
+Jakhya (Cleome viscosa)
+Jalapeño (Capsicum annuum cultivar)
+Jiaogulan (Gynostemma pentaphyllum)
+Jimbu (Allium hypsistum) (Nepal)
+Juniper berry (Juniperus communis)
+K
+Kaffir lime leaves, Makrud lime leaves (Citrus hystrix) (Southeast Asia)
+Kala zeera (or kala jira), black cumin (Bunium persicum) (South Asia)
+Kawakawa seeds (Macropiper excelsum) (New Zealand)
+Keluak, kluwak, kepayang (Pangium edule)
+Kencur, galangal, kentjur (Kaempferia galanga) (Java, Bali)
+Kinh gioi, Vietnamese balm (Elsholtzia ciliata)
+Kokam seed (Garcinia indica) (Indian confectionery)
+Korarima, Ethiopian cardamom, false cardamom (Aframomum corrorima) (Eritrea)
+Koseret leaves (Lippia abyssinica) (Ethiopia)
+Kudum Puli (Garcinia gummi-gutta)
+L
+Lavender (Lavandula spp.)
+Lemon balm (Melissa officinalis)
+Lemon ironbark (Eucalyptus staigeriana) (Australia)
+Lemon myrtle (Backhousia citriodora) (Australia)
+Lemon verbena (Lippia citriodora)
+Lemongrass (Cymbopogon citratus, C. flexuosus, and other Cymbopogon spp.)
+Leptotes bicolor (Paraguay and southern Brazil)
+Lesser calamint (Calamintha nepeta), nipitella, nepitella (Italy)
+Licorice, liquorice (Glycyrrhiza glabra)
+Lime flower, linden flower (Tilia spp.)
+Lovage (Levisticum officinale)
+Locust beans (Ceratonia siliqua)
+M
+Mace (Myristica fragrans)
+Mahleb, mahalepi, St. Lucie cherry (Prunus mahaleb)
+Marjoram (Origanum majorana)
+Mastic (Pistacia lentiscus)
+Mint (Mentha spp.), 25 species, hundreds of varieties
+Mountain horopito (Pseudowintera colorata), 'pepper-plant' (New Zealand)
+Musk mallow, abelmosk (Abelmoschus moschatus)
+Mustard, black, mustard plant, mustard seed (Brassica nigra)
+Mustard, brown, mustard plant, mustard seed (Brassica juncea)
+Mustard, white, mustard plant, mustard seed (Sinapis alba)
+Mustard, yellow (Brassica hirta = Sinapis alba)
+N
+New Mexico chile (Capsicum annuum 'New Mexico Group', also known as Hatch or Anaheim) which includes Big Jim, Chimayó, and Sandia, and other pepper cultivars.
+Nigella, black caraway, black cumin, black onion seed, kalonji (Nigella sativa)
+Njangsa, djansang (Ricinodendron heudelotii) (West Africa)
+Nutmeg (Myristica fragrans)
+O
+Olida (Eucalyptus olida) (Australia)
+Oregano (Origanum vulgare, O. heracleoticum, and other species)
+Oregano, Cuban (Coleus amboinicus)
+Oregano, Mexican (Lippia graveolens)
+Orris root (Iris germanica, I. florentina, I. pallida)
+P
+Pandan flower, kewra (Pandanus odoratissimus)
+Pandan leaf, screwpine (Pandanus amaryllifolius)
+Pápalo (Porophyllum ruderale) (Mexico and South America)
+Paprika (Capsicum annuum)
+Paracress (Acmella oleracea) (Brazil)
+Parsley (Petroselinum crispum)
+Pennyroyal (Mentha pulegium)
+Pepper, black, white, and green (Piper nigrum)
+Pepper, Brazilian, or pink pepper (Schinus terebinthifolius)
+Pepper, Dorrigo (Tasmannia stipitata) (Australia)
+Pepper, long (Piper longum)
+Pepper, mountain, Cornish pepper leaf (Tasmannia lanceolata)
+Peppermint (Mentha piperata)
+Peppermint gum leaf (Eucalyptus dives)
+Perilla (Mentha pulegium)
+Deulkkae (Perilla frutescens seeds)
+Kkaennip (Perilla frutescens leaves)
+Shiso (Perilla frutescens var. crispa leaves)
+Peruvian pepper (Schinus molle)
+Pipicha, straight-leaf pápalo (Porophyllum linaria) (Mexico)
+Poppy seed (Papaver somniferum)
+Purslane
+Q
+Quassia (Quassia amara), bitter spice in aperitifs and some beers and fortified wines
+R
+Red rice powder (Monascus purpureus) (China)
+Rice paddy herb (Limnophila aromatica) (Vietnam)
+Rosemary (Rosmarinus officinalis)
+Rue (Ruta graveolens)
+S
+Safflower (Carthamus tinctorius), only for yellow color
+Saffron (Crocus sativus)
+use of saffron
+Sage (Salvia officinalis)
+Salad burnet (Sanguisorba minor)
+Salep (Orchis mascula)
+Sassafras (Sassafras albidum)
+Sesame Seed, Black Sesame Seed
+Savory, summer (Satureja hortensis)
+Savory, winter (Satureja montana)
+Shiso (Perilla frutescens)
+Silphium, silphion, laser, laserpicium, sorado (Ancient Roman cuisine, Ancient Greek cuisine)
+Sorrel (Rumex acetosa)
+Sorrel, sheep (Rumex acetosella)
+Spearmint (Mentha spicata)
+Spikenard (Nardostachys grandiflora or N. jatamansi)
+Star anise (Illicium verum)
+Sumac (Rhus coriaria)
+Sweet woodruff (Galium odoratum)
+Szechuan pepper, Sichuan pepper (Zanthoxylum piperitum)
+T
+Tarragon (Artemisia dracunculus)
+Tasmanian pepper (Tasmannia lanceolata))
+Thyme (Thymus vulgaris)
+Thyme, lemon (Thymus citriodorus)
+Tonka beans (Dipteryx odorata)
+Turmeric (Curcuma longa)
+V
+Vanilla (Vanilla planifolia)
+Voatsiperifery (Piper borbonense) [Madagascar]
+W
+Wasabi (Wasabia japonica)
+Water-pepper, smartweed (Polygonum hydropiper)
+Watercress (Rorippa nasturtium-aquatica)
+Wattleseed (from about 120 spp. of Australian Acacia)
+Wild thyme (Thymus serpyllum)
+Willow herb (Epilobium parviflorum)
+Wintergreen (Gaultheria procumbens)
+Wood avens, herb bennet (Geum urbanum)
+Woodruff (Galium odoratum)
+Wormwood, absinthe (Artemisia absinthium)
+Y
+Yerba buena, any of four different species, many unrelated
+Yarrow (Achillea millefolium)
+Z
+Za'atar (herbs from the genera Origanum, Calamintha, Thymus, and Satureja)
+Zedoary (Curcuma zedoaria)"""}
--- web/src/init_db.py
+++ web/src/init_db.py
@@ -1,66 +1,94 @@
+from parse_ingredients import parse_ingredient
 import mysql.connector as mysql
 import csv
 # Load the credentials from the secured .env file
 import os
 from dotenv import load_dotenv
+from foodgroups import groups
 load_dotenv('credentials.env')
-from parse_ingredients import parse_ingredient
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']
-db_host = os.environ['MYSQL_HOST'] # must 'localhost' when running this script outside of Docker
+# must 'localhost' when running this script outside of Docker
+db_host = os.environ['MYSQL_HOST']
+
+
 
 with open('RecipeDatabase.csv', newline='') as csvfile:
     data = list(csv.reader(csvfile))
 data = data[1:]
 ing_recs = []
 for row in data:
-  row.append("https://img.icons8.com/cute-clipart/64/000000/bookmark-ribbon.png")
-  row[2] = str(row[2])
-  row[9] = int(row[9])
-  row[10] = int(row[10])
-  row[8] = int(row[8])
-  row[1] = int(row[1])
-  row[7] = str(row[7])
-  records = []
-  ingredients = row[3].split(';')
-  for i in ingredients:
-    ing = parse_ingredient(i)
-    records.append((str(row[0]),str(ing.name), 'unknown', ( " "+str(ing.quantity)), str(ing.unit), 3.14, ))
-    
-  ing_recs.append(records) 
+    row.append("https://img.icons8.com/cute-clipart/64/000000/bookmark-ribbon.png")
+    row[2] = str(row[2])
+    row[9] = int(row[9])
+    row[10] = int(row[10])
+    row[8] = int(row[8])
+    row[1] = int(row[1])
+    row[7] = str(row[7])
+    records = []
+    ingredients = row[3].split(';')
+    for i in ingredients:
+        ing = parse_ingredient(i)
+        food_group = 'unknown'
+        for x in (ing.name).split(' '):
+          if (x).lower() in groups['dairy'].lower():
+            food_group = 'Dairy'
+            break
+          elif (x).lower() in groups['vegetables'].lower():
+            food_group = 'Vegetable'
+            break
+          elif (x).lower() in groups['fruits'].lower():
+            food_group = 'Fruit'
+            break
+          elif (x).lower() in groups['meats'].lower():
+            food_group = 'Meat'
+            break
+          elif (x).lower() in groups['seafood'].lower():
+            food_group = 'Seafood'
+            break
+          elif (x).lower() in groups['poultry'].lower():
+            food_group = 'Poultry'
+            break
+          elif (x).lower() in groups['grains'].lower():
+            food_group = 'Grain'
+            break
+          elif (x).lower() in groups['seasonings'].lower():
+            food_group = 'Seasoning'
+            break
+        records.append((str(row[0]), str(ing.name), food_group ,
+                       (" "+str(ing.quantity)), str(ing.unit), 3.14, ))
 
-
-
-
+    ing_recs.append(records)
 
 
 with open('IngredientDatabase.csv', newline='') as csvfile:
-  idata = list(csv.reader(csvfile))
-  idata = idata[1:]
-  for irow in idata:
-    irow[0] = str(irow[0])
+    idata = list(csv.reader(csvfile))
+    idata = idata[1:]
+    for irow in idata:
+        irow[0] = str(irow[0])
 
 
 # Connect to the database
-db = mysql.connect(user=db_user, password=db_pass, host=db_host, database=db_name)
+db = mysql.connect(user=db_user, password=db_pass,
+                   host=db_host, database=db_name)
 cursor = db.cursor()
 
 # # CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
 try:
-  cursor.execute("DROP TABLE IF EXISTS Users;")
-  cursor.execute("DROP TABLE IF EXISTS Recipes;")
-  cursor.execute("DROP TABLE IF EXISTS Bookmarks;")
-  cursor.execute("DROP TABLE IF EXISTS Ingredients;")
-  cursor.execute("DROP TABLE IF EXISTS Basket;")
-  cursor.execute("DROP TABLE IF EXISTS ParsedIngredients;")
-  db.commit()
+    cursor.execute("DROP TABLE IF EXISTS Users;")
+    cursor.execute("DROP TABLE IF EXISTS Recipes;")
+    cursor.execute("DROP TABLE IF EXISTS Bookmarks;")
+    cursor.execute("DROP TABLE IF EXISTS Ingredients;")
+    cursor.execute("DROP TABLE IF EXISTS Basket;")
+    cursor.execute("DROP TABLE IF EXISTS ParsedIngredients;")
+    db.commit()
 except (mysql.Error, mysql.Warning) as e:
-  print(e, 'ERROR')
+    print(e, 'ERROR')
 
 # Create a TStudents table (wrapping it in a try-except is good practice)
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS Users (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       full_name  VARCHAR(50) NOT NULL,
@@ -70,10 +98,10 @@ try:
     );
   """)
 except:
-  print("Users table already exists. Not recreating it.")
+    print("Users table already exists. Not recreating it.")
 
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS Recipes (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       name        VARCHAR(100) NOT NULL,
@@ -94,11 +122,11 @@ try:
     );
   """)
 except (mysql.Error, mysql.Warning) as e:
-  print(e, 'ERROR')
+    print(e, 'ERROR')
 
 
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS Bookmarks (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       email       VARCHAR(100),
@@ -118,20 +146,20 @@ try:
     );
   """)
 except (mysql.Error, mysql.Warning) as e:
-  print(e, 'ERROR')
-  
+    print(e, 'ERROR')
+
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS Ingredients (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       ingredient  VARCHAR(50) NOT NULL
     );
   """)
 except:
-  print("Ingredient table already exists. Not recreating it.")
+    print("Ingredient table already exists. Not recreating it.")
 
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS Basket (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       ingredient  VARCHAR(200) NOT NULL,
@@ -143,19 +171,19 @@ try:
     );
   """)
 
-  # example record:
-  # ingredient: "cheddar cheese"
-  # category: "dairy"
-  # quantity: 8
-  # unit: "oz"
-  # cost: 4.35
-  # email "user@gmail.com"
+    # example record:
+    # ingredient: "cheddar cheese"
+    # category: "dairy"
+    # quantity: 8
+    # unit: "oz"
+    # cost: 4.35
+    # email "user@gmail.com"
 
 except:
-  print("Basket table already exists. Not recreating it.")
+    print("Basket table already exists. Not recreating it.")
 
 try:
-  cursor.execute("""
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS ParsedIngredients (
       id          integer  AUTO_INCREMENT PRIMARY KEY,
       recipe      VARCHAR(100)  not null,
@@ -167,23 +195,23 @@ try:
     );
   """)
 
-  # example record:
-  # ingredient: "cheddar cheese"
-  # category: "dairy"
-  # quantity: 8
-  # unit: "oz"
-  # cost: 4.35
-  # email "user@gmail.com"
+    # example record:
+    # ingredient: "cheddar cheese"
+    # category: "dairy"
+    # quantity: 8
+    # unit: "oz"
+    # cost: 4.35
+    # email "user@gmail.com"
 
 except:
-  print("Basket table already exists. Not recreating it.")
-  
+    print("Basket table already exists. Not recreating it.")
+
 
 # Insert Records
 query = "insert into Users (full_name, email, password) values (%s, %s,  %s);"
 values = [
-  ('Andrew Hernandez','anh190@ucsd.edu', '54Soleda')
-  
+    ('Andrew Hernandez', 'anh190@ucsd.edu', '54Soleda')
+
 ]
 
 cursor.executemany(query, values)
@@ -197,9 +225,9 @@ db.commit()
 
 for i in ing_recs:
 
-  query = """insert into ParsedIngredients (recipe,ingredient, category, quantity, unit, cost) values (%s, %s, %s, %s, %s, %s);"""
-  cursor.executemany(query, i)
-  db.commit()
+    query = """insert into ParsedIngredients (recipe,ingredient, category, quantity, unit, cost) values (%s, %s, %s, %s, %s, %s);"""
+    cursor.executemany(query, i)
+    db.commit()
 
 # Selecting Records
 cursor.execute("select * from Users;")
